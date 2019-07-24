db12ef4e2fcc65d0bde1aee7a28d52c7b92efcf9
Thor <thor.stg@gmail.com>
2019-07-24 00:07:14 +0000

    lures

--- PokeAlarm/Filters/StopFilter.py
+++ PokeAlarm/Filters/StopFilter.py
@@ -5,12 +5,14 @@ import operator
 from . import BaseFilter
 from PokeAlarm.Utilities import StopUtils as StopUtils
 
+
 class StopFilter(BaseFilter):
     """ Filter class for limiting which stops trigger a notification. """
 
     def __init__(self, mgr, name, data):
         """ Initializes base parameters for a filter. """
         super(StopFilter, self).__init__(mgr, 'egg', name)
+
         # Lures
         self.lure_ids = self.evaluate_attribute(
             event_attribute='lure_type_id', eval_func=operator.contains,
@@ -23,6 +25,7 @@ class StopFilter(BaseFilter):
             eval_func=lambda d, v: not operator.contains(d, v),
             limit=BaseFilter.parse_as_set(
                 StopUtils.get_lure_id, 'lures_exclude', data))
+
         # Distance
         self.min_dist = self.evaluate_attribute(  # f.min_dist <= m.distance
             event_attribute='distance', eval_func=operator.le,
@@ -63,6 +66,7 @@ class StopFilter(BaseFilter):
         # Lures
         if self.lure_ids is not None:
             settings['lure_ids'] = self.lure_ids
+
         # Distance
         if self.min_dist is not None:
             settings['min_dist'] = self.min_dist
