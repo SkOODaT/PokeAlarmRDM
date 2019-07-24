99ff6e267b1037c2f91a3765281d124f12920cff
Thor <thor.stg@gmail.com>
2019-07-24 00:06:30 +0000

    lute types

--- PokeAlarm/Events/StopEvent.py
+++ PokeAlarm/Events/StopEvent.py
@@ -15,20 +15,23 @@ class StopEvent(BaseEvent):
         """ Creates a new Stop Event based on the given dict. """
         super(StopEvent, self).__init__('stop')
         check_for_none = BaseEvent.check_for_none
+
         # Identification
         self.stop_id = data['pokestop_id']
-
+        
         # Details
         self.stop_name = check_for_none(
             str, data.get('pokestop_name') or data.get('name'), Unknown.REGULAR)
         self.stop_image = check_for_none(
             str, data.get('pokestop_url') or data.get('url'), Unknown.REGULAR)
         self.lure_type_id = check_for_none(int, data.get('lure_id'), 0)
+
         # Time left
         if self.lure_type_id > 0:
             self.expiration = data['lure_expiration']
         else:
             self.expiration = data['incident_expiration']
+
         self.time_left = None
         if self.expiration is not None:
             self.expiration = datetime.utcfromtimestamp(self.expiration)
@@ -54,11 +57,13 @@ class StopEvent(BaseEvent):
         dts.update({
             # Identification
             'stop_id': self.stop_id,
+            
             # Details
             'stop_name': self.stop_name,
             'stop_image': self.stop_image,
             'lure_type_id': self.lure_type_id,
             'lure_type_name': locale.get_lure_type_name(self.lure_type_id),
+
             # Time left
             'time_left': time[0],
             '12h_time': time[1],
