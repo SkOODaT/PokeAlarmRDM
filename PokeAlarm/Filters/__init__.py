a16060b1479b0a152a98cc58bae438284f0762f3
Thor <thor.stg@gmail.com>
2019-07-26 01:14:59 +0000

    Update __init__.py

--- PokeAlarm/Filters/__init__.py
+++ PokeAlarm/Filters/__init__.py
@@ -1,6 +1,7 @@
 from BaseFilter import BaseFilter  # noqa F401
 from MonFilter import MonFilter  # noqa F401
 from StopFilter import StopFilter  # noqa F401
+from InvasionFilter import InvasionFilter  # noqa F401
 from GymFilter import GymFilter  # noqa F401
 from EggFilter import EggFilter  # noqa F401
 from RaidFilter import RaidFilter  # noqa F401
